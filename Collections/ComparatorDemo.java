dbb8a5f4a004074a04fc1459292f6cb7f11a1228
Robot <androlepcha843@gmail.com>
2024-02-05 22:41:18 +0600

    Implementing comparator

---
+++ Collections/ComparatorDemo.java
@@ -0,0 +1,63 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class ComparatorDemo {
+    public static void main(String[] args) {
+
+          //Comprator is an interface so we have 3 diffrent multiple ways to implement
+        //1. by implementing in class
+        //2. by creating anonymous class
+        //3. lambda fun 
+        Comparator<Integer> com  = new Comparator<Integer>(){ // this is anonymous class implementation
+            public int compare(Integer a,Integer b){
+                if(a%10 > b%10)
+                    return 1; // It means swap
+                else
+                    return -1;
+            }
+        };
+
+        List<Integer> ls = new ArrayList<>();
+        ls.add(45);
+        ls.add(56);
+        ls.add(90);
+        ls.add(59);
+        System.out.println("Before Sorting "+ls);
+        Collections.sort(ls); // It sort based on 
+        System.out.println("After Sorting "+ls);
+        Collections.sort(ls,com);
+        //Now sort the ArrayList base on the last int value 
+        //we can use the concept of Comparator
+        System.out.println("After Sorting  based on the logic "+ls);
+
+        // ArrayList of an Student
+        List<Student> st=new ArrayList<>();
+        st.add(new Student(12,"Andro",21));
+        st.add(new Student(13,"Robot",24));
+        st.add(new Student(11,"Binita",23));
+        st.add(new Student(9,"Mehersa",16));
+        st.add(new Student(15,"Arpan",21));
+
+        // Comparator for student using anonymous class
+        Comparator<Student> srt = new Comparator<Student>() {
+            public int compare(Student a,Student b){
+                if(a.reg>b.reg)
+                    return 1;
+                else
+                return -1;
+            }
+        };
+
+        System.out.println("Student data"+st);
+        Collections.sort(st,srt);
+        for(Student s : st){
+            System.out.println(s);
+        }
+
+
+
+      
+    }
+}
---
+++ Collections/Student.java
@@ -0,0 +1,17 @@
+public class Student {
+    public int reg;
+    public String name;
+    public int age;
+
+    Student(int reg,String name,int age){
+            this.reg=reg;
+            this.name=name;
+            this.age=age;
+    }
+
+    @Override
+    public String toString() {
+        return "Student [reg=" + reg + ", name=" + name + ", age=" + age + "]";
+    }
+    
+}
